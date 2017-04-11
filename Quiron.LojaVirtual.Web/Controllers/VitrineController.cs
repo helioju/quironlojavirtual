fb0f637bc37f39dbc9619c606d5c3ca0d3a99214
Helio de Freitas Junior <hfj1974@outlook.com>
2017-04-10 22:27:46 -0300

    Teste

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,28 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosporPagina = 3;
+
+        // GET: Vitrine
+        public ActionResult ListaProdutos(int Pagina = 1)
+        {
+
+            _repositorio = new ProdutosRepositorio();
+            var Produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+                .Skip((Pagina - 1) * ProdutosporPagina)
+                .Take(ProdutosporPagina);
+
+            return View(Produtos);
+        }
+    }
+}
